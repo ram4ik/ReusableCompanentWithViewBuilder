5bc8e6749ad3460339984107df76a6433db50e08
Ramill <ramill_ibragimov@icloud.com>
2022-10-14 21:46:18 +0300

    Initial Commit

---
+++ ReusableCompanentWithViewBuilder/ReusableCompanentWithViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableCompanentWithViewBuilderApp.swift
+//  ReusableCompanentWithViewBuilder
+//
+//  Created by Ramill Ibragimov on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReusableCompanentWithViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
